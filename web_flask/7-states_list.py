de5c0862b7e109ea5b0e9331cc9faa8826406219
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-21 11:07:22 +0400

    Task 8 created

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""7-states_list.py"""
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """States List"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(ex):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
