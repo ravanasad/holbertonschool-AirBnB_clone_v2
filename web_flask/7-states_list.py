019e859e1e6ced80b6618ed2e6e21842eee7e483
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-21 11:55:23 +0400

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """7-states_list.py"""
 from models import storage
 from flask import Flask, render_template
-
+from models.state import State
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """States List"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
